3a30f92af156a5b0e41c6cbd56d5388d57e8ef0b
Goxy1 <uros.gojak001@gmail.com>
2023-05-21 18:09:12 +0200

    Code cleanup

--- AppConstants/Constants.cs
+++ AppConstants/Constants.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AutomationFramework.AppConstants
+﻿namespace AutomationFramework.AppConstants
 {
     public class Constants
     {
