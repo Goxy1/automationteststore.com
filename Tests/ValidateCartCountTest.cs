8e5ee3cda5f0e0b3d8f29826a5937a4383f4e094
Goxy1 <uros.gojak001@gmail.com>
2023-05-21 18:05:50 +0200

    Added ValidateCartCountTest/PriceTest/Comments

---
+++ Tests/ValidateCartCountTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework.Tests
+{
+    public class ValidateCartCountTest:BaseTest
+    {
+        [Test]
+        public void ValidateCartCount()
+        {
+            //Dodajemo u cart i assert da proverimo da li se broj proizvoda povecava
+            int i = 1;
+            while (i <= 3)
+            {
+                Pages.HomePage.ClickOnAddToCartButton();
+                Assert.AreEqual(i.ToString(), Pages.CartPage.GetNumberOfItemsInCart());
+                i++;
+            }
+
+            //ulazi u cart
+            Pages.HomePage.ClickCartButton();
+
+            //zbog petlje iznad je na 4 pa treba da se spusti samo nazad na 3
+            i--;
+            //brisanje iz korpe i assert da li se broj smanjuje
+            while (i > 0)
+            {
+                i--;
+                Pages.CartPage.UpdateItemQuantity(Pages.CartPage.GetNumberOfItemsInCart());
+                Assert.AreEqual(i.ToString(), Pages.CartPage.GetNumberOfItemsInCart());
+            }
+        }
+    }
+}
---
+++ Tests/ValidateCartPriceTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFramework.Tests
+{
+    public class ValidateCartPriceTest:BaseTest
+    {
+        [Test]
+        public void ValidateCartPrice()
+        {
+            
+            Pages.HomePage.AddTwoItemsToCart();
+            Pages.HomePage.ClickCartButton();
+
+            //cena koja pise na sajtu i cena koja je dobijena sabiranjem cena proizvoda
+            float actualTotalPrice = Pages.CartPage.GetActualTotalPrice();
+            //ocekivana cena
+            float expectedTotalPrice = Pages.CartPage.GetExpectedTotalPrice();
+            //Proveravamo da li su iste cene
+            Assert.AreEqual(expectedTotalPrice, actualTotalPrice);
+
+            //brisemo prvi proizvod iz korpe
+            Pages.CartPage.RemoveFirstItemFromCart();
+            //cena koja pise na sajtu i cena koja je dobijena sabiranjem cena proizvoda
+            float newActualTotalPrice = Pages.CartPage.GetActualTotalPrice();
+            //nova cena koju ocekujemo
+            float newExpectedTotalPrice = Pages.CartPage.GetExpectedTotalPrice();
+            //Proveravamo da li su cene iste
+            Assert.AreEqual(newExpectedTotalPrice, newActualTotalPrice);
+        }
+    }
+}
